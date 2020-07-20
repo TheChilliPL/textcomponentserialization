ba9f5686a5882f963287b60d4199679f9863cd02
Patryk Anuszczyk <dragonacchilli@gmail.com>
2020-07-20 20:13:57 +0200

    Add getBook and setBook methods

--- src/main/kotlin/me/patrykanuszczyk/textcomponentserialization/configurationExtension.kt
+++ src/main/kotlin/me/patrykanuszczyk/textcomponentserialization/configurationExtension.kt
@@ -2,13 +2,14 @@ package me.patrykanuszczyk.textcomponentserialization
 
 import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.inventory.ItemStack
 
 fun ConfigurationSection.getTextComponent(path: String): TextComponent? {
     return deserializeTextComponent(this.get(path))
 }
 
-fun ConfigurationSection.setTextComponent(path: String, value: TextComponent? = null) {
-    this.set(path, value)
+fun ConfigurationSection.setTextComponent(path: String, value: TextComponent?) {
+    this.set(path, value.serialize())
 }
 
 fun ConfigurationSection.getTextComponentList(path: String): List<TextComponent?> {
@@ -17,4 +18,12 @@ fun ConfigurationSection.getTextComponentList(path: String): List<TextComponent?
 
 fun ConfigurationSection.setTextComponentList(path: String, value: List<TextComponent?>) {
     this.set(path, value.map { it.serialize() })
+}
+
+fun ConfigurationSection.getBook(path: String): ItemStack? {
+    return deserializeBook(this.get(path))
+}
+
+fun ConfigurationSection.setBook(path: String, value: ItemStack?) {
+    this.set(path, serializeBook(value))
 }
