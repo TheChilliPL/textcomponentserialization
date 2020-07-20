296b8b5bb677bd6c7e9796886712b2e330b93c56
Patryk Anuszczyk <dragonacchilli@gmail.com>
2020-07-20 19:58:57 +0200

    Add extension for ConfigurationSection, add book serialization, add comments

--- src/main/kotlin/me/patrykanuszczyk/textcomponentserialization/serialize.kt
+++ src/main/kotlin/me/patrykanuszczyk/textcomponentserialization/serialize.kt
@@ -4,42 +4,49 @@ import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.chat.ClickEvent
 import net.md_5.bungee.api.chat.HoverEvent
 import net.md_5.bungee.api.chat.TextComponent
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BookMeta
 
+/**
+ * Serializes a text component.
+ * @since 1.0
+ */
 @JvmName("serializeTextComponent")
 fun TextComponent?.serialize(): Any? {
-    if(this == null) return null
+    if (this == null) return null
 
     val map = mutableMapOf<String, Any>()
 
     if (!text.isNullOrEmpty())
-        map += "text" to text
+        map["text"] = text
 
     if (!extra.isNullOrEmpty())
-        map += "extra" to extra.map { (it as TextComponent).serialize() }
+        map["extra"] = extra.map { (it as TextComponent).serialize() }
 
     if (colorRaw != null)
-        map += "color" to serializeChatColor(colorRaw)!!
+        map["color"] = serializeChatColor(colorRaw)!!
     if (isBoldRaw != null)
-        map += "bold" to isBoldRaw
+        map["bold"] = isBoldRaw
     if (isItalicRaw != null)
-        map += "italic" to isItalicRaw
+        map["italic"] = isItalicRaw
     if (isUnderlinedRaw != null)
-        map += "underlined" to isUnderlinedRaw
+        map["underlined"] = isUnderlinedRaw
     if (isStrikethroughRaw != null)
-        map += "strikethrough" to isStrikethroughRaw
+        map["strikethrough"] = isStrikethroughRaw
     if (isObfuscatedRaw != null)
-        map += "obfuscated" to isObfuscatedRaw
+        map["obfuscated"] = isObfuscatedRaw
 
     if (insertion != null)
-        map += "insertion" to insertion
+        map["insertion"] = insertion
 
     if (clickEvent != null) {
-        map += "clickEvent" to serializeClickEvent(
+        map["clickEvent"] = serializeClickEvent(
             clickEvent
         )!!
     }
     if (hoverEvent != null) {
-        map += "hoverEvent" to serializeHoverEvent(
+        map["hoverEvent"] = serializeHoverEvent(
             hoverEvent
         )!!
     }
@@ -49,23 +56,31 @@ fun TextComponent?.serialize(): Any? {
             "text" -> return map.getValue("text")
             "extra" -> return map.getValue("extra")
         }
-    } else if(map.keys.isEmpty()) {
+    } else if (map.keys.isEmpty()) {
         return ""
     }
 
     return map
 }
 
+/**
+ * Serializes a chat color.
+ * @since 1.0
+ */
 fun serializeChatColor(color: ChatColor?): Any? {
-    if(color == null) return null
+    if (color == null) return null
 
     return color.name
         .replace('_', ' ')
         .toLowerCase()
 }
 
+/**
+ * Serializes a click event.
+ * @since 1.0
+ */
 fun serializeClickEvent(event: ClickEvent?): Any? {
-    if(event == null) return null
+    if (event == null) return null
 
     val action = event.action.name
         .replace('_', ' ')
@@ -79,8 +94,12 @@ fun serializeClickEvent(event: ClickEvent?): Any? {
     )
 }
 
+/**
+ * Serializes a hover event.
+ * @since 1.0
+ */
 fun serializeHoverEvent(event: HoverEvent?): Any? {
-    if(event == null) return null
+    if (event == null) return null
 
     val action = event.action.name
         .replace('_', ' ')
@@ -88,7 +107,7 @@ fun serializeHoverEvent(event: HoverEvent?): Any? {
 
     val values = event.value
 
-    val component = (if(values.size == 1)
+    val component = (if (values.size == 1)
         values[0]
     else
         TextComponent(*values)) as TextComponent
@@ -99,4 +118,45 @@ fun serializeHoverEvent(event: HoverEvent?): Any? {
         "action" to action,
         "value" to value
     )
+}
+
+/**
+ * Serializes a book item stack.
+ * @since 1.0
+ */
+fun serializeBook(stack: ItemStack?): Any? {
+    if (stack == null) return null
+    require(stack.type == Material.WRITTEN_BOOK) { "Book has to be of type WRITTEN_BOOK." }
+
+    val bookMeta = stack.itemMeta as BookMeta
+    val map = mutableMapOf<String, Any>()
+
+    map["title"] = bookMeta.title
+    map["author"] = bookMeta.author
+    map["generation"] = serializeBookGeneration(bookMeta.generation)!!
+
+    map["pages"] = bookMeta.spigot().pages
+        .map {
+            if (it.size == 1)
+                it[0] as TextComponent
+            else
+                TextComponent(*it)
+        }
+        .map {
+            it.serialize()
+        }
+
+    return map
+}
+
+/**
+ * Serializes book generation.
+ * @since 1.0
+ */
+fun serializeBookGeneration(generation: BookMeta.Generation?): Any? {
+    if (generation == null) return null
+
+    return generation.name
+        .replace('_', ' ')
+        .toLowerCase()
 }
