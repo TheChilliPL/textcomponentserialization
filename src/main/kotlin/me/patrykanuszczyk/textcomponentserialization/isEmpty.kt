3f59367e484f9123f6af22bc50605020650cc3b4
Patryk Anuszczyk <dragonacchilli@gmail.com>
2020-07-26 03:12:51 +0200

    Fix isEmpty

--- src/main/kotlin/me/patrykanuszczyk/textcomponentserialization/isEmpty.kt
+++ src/main/kotlin/me/patrykanuszczyk/textcomponentserialization/isEmpty.kt
@@ -4,7 +4,7 @@ import net.md_5.bungee.api.chat.TextComponent
 
 val TextComponent.isEmpty: Boolean
     get() {
-        if(!text.isNullOrEmpty()) return true
+        if(!text.isNullOrEmpty()) return false
 
         return extra.all { it is TextComponent && it.isEmpty }
     }
